beabe29343186a5fe72854a40590283ec50ec515
cafe small house <ybaram40@gmail.com>
2026-03-01 10:29:51 +0900

    #7.4 ForeignKey Filter (11:33)

--- reviews/models.py
+++ reviews/models.py
@@ -8,12 +8,11 @@ from common.models import CommonModel
 class Review(CommonModel):
     """Review Model Definition"""
 
+    payload = models.TextField()
+    rating = models.PositiveIntegerField()
     user = models.ForeignKey('users.User', on_delete=models.CASCADE)
     room = models.ForeignKey('rooms.Room', on_delete=models.CASCADE, null=True, blank=True)
     experience = models.ForeignKey('experiences.Experience', on_delete=models.CASCADE, null=True, blank=True)
-    payload = models.TextField()
-
-    rating = models.PositiveIntegerField()
 
     def __str__(self):
         return f'{self.room} / {self.rating}'
