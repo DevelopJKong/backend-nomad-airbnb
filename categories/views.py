a4619ce67c4dfc0604f684c5cd6a8d4d044e696c
cafe small house <ybaram40@gmail.com>
2026-03-31 23:23:58 +0900

    #10.3 Serializer (10:53)

--- categories/views.py
+++ categories/views.py
@@ -1,10 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.views import Response
 
+from categories.serializers import CategorySerializer
+
 from .models import Category
 
 
 @api_view(['GET'])
 def categories(request):
     all_categories = Category.objects.all()
-    return Response({'ok': True, 'categories': all_categories})
+    serializer = CategorySerializer(all_categories, many=True)
+    return Response({'ok': True, 'categories': serializer.data})
