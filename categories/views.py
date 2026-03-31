89440780747025a54034b21e673e80d9982d5679
cafe small house <ybaram40@gmail.com>
2026-03-31 23:08:21 +0900

    #10.1 JsonResponse (12:39)

--- categories/views.py
+++ categories/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.http import JsonResponse
+
+from .models import Category
+
+
+def categories(request):
+    all_categories = Category.objects.all()
+    return JsonResponse({'ok': True, 'categories': all_categories})
